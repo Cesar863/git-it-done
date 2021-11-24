cc85cfe1446bcb9478eecccf2f12e88e45b9ddde
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2021-11-23 22:49:09 -0500

    allowd issues to be visable via second page. and clicking an issue will lead to git hub website for more information about that issue, This will also allow for a user to distinguish between a pull request and a actual issue. the website will also display if the repo has more than 30 issues directing to the github website

---
+++ assets/js/single.js
@@ -0,0 +1,77 @@
+var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEL = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEL.textContent = "To see more than 30 issues, Visit ";
+
+    var linkEl = document.querySelector("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEL.appendChild(linkEl);
+};
+
+var getRepoIssues = function(repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    fetch(apiUrl).then(function(response) {
+        //request was sucessful
+        if (response.ok) {
+            response.json().then(function(data) {
+                //pass response data to dom function
+                displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
+        }
+        else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
+
+var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+
+    // create a link element to take users to the issue on git hub
+    for (var i = 0; i < issues.length; i++) {
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+    
+    // create span to hold the title
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+
+    //append to container
+    issueEl.appendChild(titleEl);
+
+    //create a type of element
+    var typeEl = document.createElement("span");
+
+    //check if issue is actual issue or pull request
+    if (issues[i].pull_request) {
+        typeEl.textContent = "(Pull Request)";
+    } else {
+        typeEl.textContent= "(Issue)";
+    }
+    //append to container
+    issueContainerEl.appendChild(issueEl);
+
+    //append to container
+    issueEl.appendChild(typeEl);
+}
+};
+
+getRepoIssues("facebook/react");
